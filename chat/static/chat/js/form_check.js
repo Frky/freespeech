92dd0bf0b003663814ec1b7f3fa534f3d1a1aa11
Franck De Goër <degoerdf@ensimag.fr>
2014-02-14 11:37:01 +0100

    Add form validation client side

---
+++ chat/static/chat/js/form_check.js
@@ -0,0 +1,42 @@
+/***
+        This module handles all form submissions and check a first time 
+        the validity of the form. Of course, the form is checked once again 
+        on the server side.
+  ***/
+
+var checkComptoirForm = function(form) {
+
+    var valid = true;
+
+    /* Checking Comptoir title */
+    if ($("#id_title", form).val() === "") {
+        $("#id_title").parent().addClass("has-error");
+        valid = false;
+    } else {
+        $("#id_title").parent().removeClass("has-error");
+    }
+
+
+    if ($("#id_public").is(":checked")) {
+        $("#comptoir-key-hash").val(CryptoJS.SHA3("ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"));
+    } else if ($("#comptoir-key-hash", form).val() === "") {
+        $("#comptoir-key-hash").parent().addClass("has-error");
+        valid = false;
+    } else {
+        $("#comptoir-key-hash").parent().removeClass("has-error");
+    }
+
+    return valid;
+}
+
+$("#id_public").change(function() {
+    $("#id_key_hash-container").toggle("hidden");
+});
+
+$("#comptoir-form").submit(function() {
+    if (!checkComptoirForm($( this ))) {
+        event.preventDefault();
+    }
+});
+
+$("#id_key_hash-container").toggle("hidden");
