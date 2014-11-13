a90db5d1b04c613f9f6469339c840354ddc01f57
Christophe Blefari <Blef@frimac-02.local>
2014-11-13 19:03:21 +0100

    Particles draft

--- chat/static/chat/js/init_page_elements.js
+++ chat/static/chat/js/init_page_elements.js
@@ -160,4 +160,41 @@ $(document).ready(function() {
     }).on('focusout', function() {
         $('.hint.betakey').hide()
     });
+
+    particlesJS('particle', {
+        particles: {
+            color: '#fff',
+            shape: 'triangle', // "circle", "edge" or "triangle"
+            opacity: 0.5,
+            size: 2,
+            size_random: true,
+            nb: 300,
+            line_linked: {
+                enable_auto: true,
+                distance: 250,
+                color: '#fff',
+                opacity: 0.5,
+                width: 1,
+                condensed_mode: {
+                    enable: true,
+                    rotateX: 600,
+                    rotateY: 600
+                }
+            },
+            anim: {
+                enable: true,
+                speed: 2
+            }
+        },
+        interactivity: {
+            enable: false,
+            mouse: {
+                distance: 250
+            },
+            detect_on: 'canvas', // "canvas" or "window"
+            mode: 'grab'
+        },
+        /* Retina Display Support */
+        retina_detect: false
+    });
 });
